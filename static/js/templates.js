70d8cad885ec5e10fe516d8fd7c7ba6902e888e8
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-02-11 22:59:28 +0100

    Refactoring: smaller methods extracted.

--- static/js/templates.js
+++ static/js/templates.js
@@ -11,43 +11,18 @@ export let templates = {
         let newCardButton = templates.createNewCardButton();
         boardHeader.appendChild(newCardButton);
         newCardButton.addEventListener('click', function(event){
-            templates.handleNewCardButtonClick(boardHeader);
+            templates.handleNewCardButtonClick(event);
         });
 
-        const boardBody = document.createElement("div");
-        boardBody.classList.add('board-body');
-
-        let table = document.createElement('table');
-        table.classList.add('board-data');
-
-        const tableHeader = document.createElement('tr');
-        tableHeader.classList.add('statuses');
-        for(const status of boardStatuses){
-            let cell = document.createElement('th');
-            cell.innerHTML = `${status}`;
-            tableHeader.appendChild(cell);
-        }
-
-        let tableBody = document.createElement('tr');
-        tableBody.classList.add('cards');
-        for(let i = 0; i < boardStatuses.length; i++){
-            let cell = document.createElement('td');
-            tableBody.appendChild(cell);
-        }
-
-        table.appendChild(tableHeader);
-        table.appendChild(tableBody);
-
-        boardBody.appendChild(table);
+        let boardBody = templates.createBoardBody(boardStatuses);
 
         board.appendChild(boardHeader);
         board.appendChild(boardBody);
 
         fullContent.appendChild(board);
-
     },
     createBoardHeader: function(boardTitle) {
-        const boardHeader = document.createElement("div");
+        let boardHeader = document.createElement("div");
         boardHeader.classList.add('board-header');
         boardHeader.innerHTML = `
             <span class="board-title">${boardTitle}</span>`;
@@ -59,13 +34,11 @@ export let templates = {
         newCardButton.innerHTML = 'Add New Card';
         return newCardButton
     },
-    handleNewCardButtonClick: function(event){
-        const newCard = templates.createCardElement('Test Card');
-        //boardHeader.appendChild(newCard);
+    handleNewCardButtonClick: function(){
+        let newCard = templates.createCardElement('Test Card');
         let board = document.querySelector(".board");
         let boardId = board.dataset.boardId;
-        //const boardId = event.target.parentNode.parentNode.dataset.boardId;
-        const firstColumn = document.querySelector(`.board[data-board-id="${boardId}"] .cards`).childNodes[0];
+        let firstColumn = document.querySelector(`.board[data-board-id="${boardId}"] .cards > :first-child`);
         firstColumn.appendChild(newCard);
     },
     createCardElement: function(cardTitle){
@@ -73,5 +46,38 @@ export let templates = {
         cardElement.classList.add('card');
         cardElement.innerHTML = `${cardTitle}`;
         return cardElement;
+    },
+    createBoardBody: function(boardStatuses){
+        let boardBody = document.createElement("div");
+        boardBody.classList.add('board-body');
+
+        let table = document.createElement('table');
+        table.classList.add('board-data');
+        let tableHeader = templates.createTableHeader(boardStatuses);
+        let tableBody = templates.createTableBody(boardStatuses);
+        table.appendChild(tableHeader);
+        table.appendChild(tableBody);
+
+        boardBody.appendChild(table);
+        return boardBody
+    },
+    createTableHeader: function(boardStatuses){
+        let tableHeader = document.createElement('tr');
+        tableHeader.classList.add('statuses');
+        for(const status of boardStatuses){
+            let cell = document.createElement('th');
+            cell.innerHTML = `${status}`;
+            tableHeader.appendChild(cell);
+        }
+        return tableHeader
+    },
+    createTableBody: function(boardStatuses){
+        let tableBody = document.createElement('tr');
+        tableBody.classList.add('cards');
+        for(let i = 0; i < boardStatuses.length; i++){
+            let cell = document.createElement('td');
+            tableBody.appendChild(cell);
+        }
+        return tableBody
     }
 };
