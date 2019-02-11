e24d0ed50000b1c6d5c99395fc28aef3a2c3d89f
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-02-11 22:27:10 +0100

    Refactoring started.

--- static/js/templates.js
+++ static/js/templates.js
@@ -1,38 +1,27 @@
 export let templates = {
-    createBoardElement: function (boardTitle, boardStatuses, boardid) {
+    createBoardElement: function (boardTitle, boardStatuses, boardId) {
 
         let fullContent = document.querySelector('#full-content');
 
         let board = document.createElement('div');
-        board.setAttribute('class', 'board');
-        board.dataset.boardId = boardid;
+        board.classList.add('board');
+        board.dataset.boardId = boardId;
 
-        const boardHeader = document.createElement("div");
-        boardHeader.setAttribute('class', 'board-header');
-        boardHeader.innerHTML = `
-            <span class="board-title">${boardTitle}</span>`;
-
-        let newCardButton = document.createElement('button');
-        newCardButton.setAttribute('class', 'new-card-button');
-        newCardButton.innerHTML = 'Add New Card';
+        let boardHeader = templates.createBoardHeader(boardTitle);
+        let newCardButton = templates.createNewCardButton();
+        boardHeader.appendChild(newCardButton);
         newCardButton.addEventListener('click', function(event){
-            const newCard = templates.createCardElement('Test Card');
-            boardHeader.appendChild(newCard);
-            const boardId = event.target.parentNode.parentNode.dataset.boardId;
-            const firstColumn = document.querySelector(`.board[data-board-id="${boardId}"] .cards`).childNodes[0];
-            firstColumn.appendChild(newCard);
+            templates.handleNewCardButtonClick(boardHeader);
         });
 
-        boardHeader.appendChild(newCardButton);
-
         const boardBody = document.createElement("div");
-        boardBody.setAttribute('class', 'board-body');
+        boardBody.classList.add('board-body');
 
         let table = document.createElement('table');
-        table.setAttribute('class', 'board-data');
+        table.classList.add('board-data');
 
         const tableHeader = document.createElement('tr');
-        tableHeader.setAttribute('class', 'statuses');
+        tableHeader.classList.add('statuses');
         for(const status of boardStatuses){
             let cell = document.createElement('th');
             cell.innerHTML = `${status}`;
@@ -40,7 +29,7 @@ export let templates = {
         }
 
         let tableBody = document.createElement('tr');
-        tableBody.setAttribute('class', 'cards');
+        tableBody.classList.add('cards');
         for(let i = 0; i < boardStatuses.length; i++){
             let cell = document.createElement('td');
             tableBody.appendChild(cell);
@@ -57,12 +46,32 @@ export let templates = {
         fullContent.appendChild(board);
 
     },
+    createBoardHeader: function(boardTitle) {
+        const boardHeader = document.createElement("div");
+        boardHeader.classList.add('board-header');
+        boardHeader.innerHTML = `
+            <span class="board-title">${boardTitle}</span>`;
+        return boardHeader
+    },
+    createNewCardButton: function(){
+        let newCardButton = document.createElement('button');
+        newCardButton.classList.add('new-card-button');
+        newCardButton.innerHTML = 'Add New Card';
+        return newCardButton
+    },
+    handleNewCardButtonClick: function(event){
+        const newCard = templates.createCardElement('Test Card');
+        //boardHeader.appendChild(newCard);
+        let board = document.querySelector(".board");
+        let boardId = board.dataset.boardId;
+        //const boardId = event.target.parentNode.parentNode.dataset.boardId;
+        const firstColumn = document.querySelector(`.board[data-board-id="${boardId}"] .cards`).childNodes[0];
+        firstColumn.appendChild(newCard);
+    },
     createCardElement: function(cardTitle){
-
         let cardElement = document.createElement('div');
-        cardElement.setAttribute('class', 'card');
+        cardElement.classList.add('card');
         cardElement.innerHTML = `${cardTitle}`;
-
         return cardElement;
     }
 };
