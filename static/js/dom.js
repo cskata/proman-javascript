111b5b5ea9c5a0965476703600938ba8647eab39
Katalin Csortos <katalin.csortos@gmail.com>
2019-02-12 15:51:09 +0100

    feat: table can be opened and closed

--- static/js/dom.js
+++ static/js/dom.js
@@ -2,59 +2,93 @@
 import {templates} from "./templates.js";
 
 export let dom = {
-    loadBoards: function () {
-        // retrieves boards and makes showBoards called
-    },
-    showBoards: function (boards) {
-        // shows boards appending them to #boards div
-        // it adds necessary event listeners also
-        for (let board of boards){
-            templates.createBoardElement(board.title,board.statuses, board.id);
-            dom.showCards(board);
-        }
-    },
-    loadCards: function (boardId) {
-        // retrieves cards and makes showCards called
-    },
-    showCards: function (board) {
-        // shows the cards of a board
-        // it adds necessary event listeners also
-        let cards = board.cards;
-        for (let i = 0; i < cards.length; i++) {
-            let column = dom.addCard(board, cards[i]);
-            let cardElement = templates.createCardElement(cards[i].title);
-            column.appendChild(cardElement);
-        }
-    },
-    appendToElement: function (elementToExtend, textToAppend, prepend = false) {
-        // function to append new DOM elements (represented by a string) to an existing DOM element
-        let fakeDiv = document.createElement('div');
-        fakeDiv.innerHTML = textToAppend.trim();
+        loadBoards: function () {
+            // retrieves boards and makes showBoards called
+        },
+        showBoards: function (boards) {
+            // shows boards appending them to #boards div
+            // it adds necessary event listeners also
+            for (let board of boards) {
+                templates.createBoardElement(board.title, board.statuses, board.id);
+                dom.showCards(board);
+            }
+        },
+        loadCards: function (boardId) {
+            // retrieves cards and makes showCards called
+        },
+        showCards: function (board) {
+            // shows the cards of a board
+            // it adds necessary event listeners also
+            let cards = board.cards;
+            for (let i = 0; i < cards.length; i++) {
+                let column = dom.addCard(board, cards[i]);
+                let cardElement = templates.createCardElement(cards[i].title);
+                column.appendChild(cardElement);
+            }
+        },
+        appendToElement: function (elementToExtend, textToAppend, prepend = false) {
+            // function to append new DOM elements (represented by a string) to an existing DOM element
+            let fakeDiv = document.createElement('div');
+            fakeDiv.innerHTML = textToAppend.trim();
 
-        for (childNode of fakeDiv.childNodes) {
-            if (prepend) {
-                elementToExtend.prependChild(childNode);
+            for (childNode of fakeDiv.childNodes) {
+                if (prepend) {
+                    elementToExtend.prependChild(childNode);
+                } else {
+                    elementToExtend.appendChild(childNode);
+                }
+            }
+
+            return elementToExtend.lastChild;
+        },
+        addCard: function (board, card) {
+            let column;
+            if (card.status_id === 1) {
+                column = document.querySelector(`.board[data-board-id='${board.id}'] .cards > :first-child`);
+            }
+            if (card.status_id === 2) {
+                column = document.querySelector(`.board[data-board-id='${board.id}'] .cards > :nth-child(2)`);
+            }
+            if (card.status_id === 3) {
+                column = document.querySelector(`.board[data-board-id='${board.id}'] .cards > :nth-child(3)`);
+            }
+            if (card.status_id === 4) {
+                column = document.querySelector(`.board[data-board-id='${board.id}'] .cards > :last-child`);
+            }
+            return column
+        },
+        toggleBoard: function (event) {
+            let boardHeader = event.target;
+            const tableContainer = boardHeader.nextSibling;
+            tableContainer.style.height = `${tableContainer.offsetHeight}px`;
+            const table = tableContainer.querySelector('.board-data');
+            const arrow = boardHeader.querySelector('.fas');
+            const newCardButton = boardHeader.querySelector('.new-card-button');
+
+            let tableHeightChecked = boardHeader.dataset.heightChecked;
+            let isTableOpen = boardHeader.dataset.open;
+            let initHeight = boardHeader.dataset.initHeight;
+
+            if (tableHeightChecked === 'false') {
+                boardHeader.dataset.initHeight = `${tableContainer.offsetHeight}`;
+                boardHeader.dataset.heightChecked = 'true';
+            }
+            if (isTableOpen === 'true') {
+                boardHeader.dataset.open = 'false';
+                tableContainer.style.height = '0px';
+                table.style.display = "none";
+                newCardButton.style.visibility = "hidden";
+                arrow.style.transform = "translate(0, 25%) rotateX(180deg)";
             } else {
-                elementToExtend.appendChild(childNode);
+                boardHeader.dataset.open = 'true';
+                tableContainer.style.height = `${initHeight}px`;
+                setTimeout(function () {
+                    table.style.display = "table";
+                    newCardButton.style.visibility = "visible";
+                }, 600);
+
+                arrow.style.transform = "rotateX(0deg)";
             }
         }
-
-        return elementToExtend.lastChild;
-    },
-    addCard: function(board, card) {
-        let column;
-        if (card.status_id === 1) {
-            column = document.querySelector(`.board[data-board-id='${board.id}'] .cards > :first-child`);
-        }
-        if (card.status_id === 2) {
-            column = document.querySelector(`.board[data-board-id='${board.id}'] .cards > :nth-child(2)`);
-        }
-        if (card.status_id === 3) {
-            column = document.querySelector(`.board[data-board-id='${board.id}'] .cards > :nth-child(3)`);
-        }
-        if (card.status_id === 4) {
-            column = document.querySelector(`.board[data-board-id='${board.id}'] .cards > :last-child`);
-        }
-        return column
     }
-};
+;
--- static/js/templates.js
+++ static/js/templates.js
@@ -1,3 +1,5 @@
+import {dom} from "./dom.js";
+
 export let templates = {
     createBoardElement: function (boardTitle, boardStatuses, boardId) {
 
@@ -10,7 +12,7 @@ export let templates = {
         let boardHeader = templates.createBoardHeader(boardTitle);
         let newCardButton = templates.createNewCardButton();
         boardHeader.appendChild(newCardButton);
-        newCardButton.addEventListener('click', function(event){
+        newCardButton.addEventListener('click', function (event) {
             templates.handleNewCardButtonClick(event);
         });
 
@@ -21,33 +23,40 @@ export let templates = {
 
         fullContent.appendChild(board);
     },
-    createBoardHeader: function(boardTitle) {
+    createBoardHeader: function (boardTitle) {
         let boardHeader = document.createElement("div");
         boardHeader.classList.add('board-header');
         boardHeader.innerHTML = `
-            <span class="board-title">${boardTitle}</span>`;
+            <span class="board-title">${boardTitle}</span>
+            <i class="fas fa-caret-up"></i>
+            `;
+
+        boardHeader.dataset.open = 'true';
+        boardHeader.dataset.heightChecked = 'false';
+        boardHeader.dataset.initHeight = '0';
+        boardHeader.addEventListener('click', dom.toggleBoard);
         return boardHeader
     },
-    createNewCardButton: function(){
+    createNewCardButton: function () {
         let newCardButton = document.createElement('button');
         newCardButton.classList.add('new-card-button');
         newCardButton.innerHTML = 'Add New Card';
         return newCardButton
     },
-    handleNewCardButtonClick: function(){
+    handleNewCardButtonClick: function () {
         let newCard = templates.createCardElement('Test Card');
         let board = document.querySelector(".board");
         let boardId = board.dataset.boardId;
         let firstColumn = document.querySelector(`.board[data-board-id="${boardId}"] .cards > :first-child`);
         firstColumn.appendChild(newCard);
     },
-    createCardElement: function(cardTitle){
+    createCardElement: function (cardTitle) {
         let cardElement = document.createElement('div');
         cardElement.classList.add('card');
         cardElement.innerHTML = `${cardTitle}`;
         return cardElement;
     },
-    createBoardBody: function(boardStatuses){
+    createBoardBody: function (boardStatuses) {
         let boardBody = document.createElement("div");
         boardBody.classList.add('board-body');
 
@@ -61,20 +70,20 @@ export let templates = {
         boardBody.appendChild(table);
         return boardBody
     },
-    createTableHeader: function(boardStatuses){
+    createTableHeader: function (boardStatuses) {
         let tableHeader = document.createElement('tr');
         tableHeader.classList.add('statuses');
-        for(const status of boardStatuses){
+        for (const status of boardStatuses) {
             let cell = document.createElement('th');
             cell.innerHTML = `${status}`;
             tableHeader.appendChild(cell);
         }
         return tableHeader
     },
-    createTableBody: function(boardStatuses){
+    createTableBody: function (boardStatuses) {
         let tableBody = document.createElement('tr');
         tableBody.classList.add('cards');
-        for(let i = 0; i < boardStatuses.length; i++){
+        for (let i = 0; i < boardStatuses.length; i++) {
             let cell = document.createElement('td');
             cell.dataset.statusId = `${i + 1}`;
             tableBody.appendChild(cell);
