ef30ef916dd2bffbc7afc09f0ab405eb26b2b898
Brigitta Arvay <arvay.brigitta22@gmail.com>
2019-02-14 13:33:56 +0100

    Deleted dead code and comments

--- static/js/dom.js
+++ static/js/dom.js
@@ -1,45 +1,19 @@
-// It uses data_handler.js to visualize elements
 import {templates} from "./templates.js";
 
 export let dom = {
-        loadBoards: function () {
-            // retrieves boards and makes showBoards called
-        },
         showBoards: function (boards) {
-            // shows boards appending them to #boards div
-            // it adds necessary event listeners also
             for (let board of boards) {
                 templates.createBoardElement(board.title, board.statuses, board.id);
                 dom.showCards(board);
             }
         },
-        loadCards: function (boardId) {
-            // retrieves cards and makes showCards called
-        },
         showCards: function (board) {
-            // shows the cards of a board
-            // it adds necessary event listeners also
             let cards = board.cards;
             for (let i = 0; i < cards.length; i++) {
                 let column = dom.addCard(board, cards[i]);
                 let cardElement = templates.createCardElement(cards[i].title, cards[i].id, cards[i].order_num);
                 column.appendChild(cardElement);
             }
-        },
-        appendToElement: function (elementToExtend, textToAppend, prepend = false) {
-            // function to append new DOM elements (represented by a string) to an existing DOM element
-            let fakeDiv = document.createElement('div');
-            fakeDiv.innerHTML = textToAppend.trim();
-
-            for (childNode of fakeDiv.childNodes) {
-                if (prepend) {
-                    elementToExtend.prependChild(childNode);
-                } else {
-                    elementToExtend.appendChild(childNode);
-                }
-            }
-
-            return elementToExtend.lastChild;
         },
         addCard: function (board, card) {
             let column;
--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,6 @@
 import {dataHandler} from "./data_handler.js";
 
-// This function is to initialize the application
 function init() {
-    // init data
-    dataHandler.init();
-    // loads the boards to the screen
     dataHandler.getBoards();
 
     const addNewBoard = document.querySelector('#new-board-button');
